a5aca8627abf116a162bf06651f7c878d2d4a7e7
PinoMacri <pinonegozio1@outlook.it>
2022-12-06 16:40:53 +0100

    Struttura e Funzionalità Esercizio Finita

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
-////console.log ('JS OK')
-//~-----------------------------------------------------------------------------------------------------------------------------------------------------------~//
-//!GENERAL
-//^ TRACCIA
-/* - Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
-Questo richiederà un minimo di ricerca. - */
-//~------------------------------------------------~//
-//^ ANALISI
-//* 1 - Prendere l'elemento dal DOM
-//* 2 - Chiedere all'utente il numero di KM che vuole percorrere E la sua età
-//* 3 - Calcoliamo il prezzo totale del viaggio:
-//- (3A) In base alla risposta che l'utente ci ha dato moltiplichiamo il valore della distanza per 0.21.
-//- (3B) Al prezzo totale del punto 3A sottraiamo il 20%, che è lo sconto riservato ai Minorenni.
-//- (3C) Al prezzo totale del punto 3A sottraiamo  il 40%, che è lo sconto riservati agli Over 65.
-//* 4 - Il prezzo finale dovrà uscire con massimo due decimali per indicare i centesimi.
-//* 5 - Pubblica i prezzi sul DOM
-//~-----------------------------------------------------------------------------------------------------------------------------------------------------------~//
-//!WORK
-//* 1 - PRENDERE GLI ELEMENTI DAL DOM
-const nome = document.getElementById("nomeCognome");
-const kmPercorrere = document.getElementById("kmPercorsi")
-const fasciaEta = document.getElementById("eta")
-const generatore = document.getElementById("generatoreS")
-//~------------------------------------------------~//
-//* 3&4 - CALCOLIAMO IL PREZZO TOTALE DEL VIAGGIO
-// - General Variable
+const nomePassegero = document.getElementById("nomePassegero")
+const input = document.getElementById("input")
+const generatore = document.getElementById("generatore")
+const numeroKm = document.getElementById("numeroKm")
+const eta = document.getElementById("eta")
+let button = document.getElementById("button")
+
 const max = 18;
 const min = 65;
 const scontoMinorenne = 20;
 const scontoOver = 40;
 const absolutePercentual = 100;
 const euro = "€"
-//* MILESTONE
-const button = document.getElementById("button")
-button.addEventListener ("click" , function(){
-generatore.innerText = "LALLA";
+
+
+
+button.addEventListener("click" , function (){
+    const text = input.value.trim();
+    nomePassegero.innerText = text;
+    input.value = "";
 })
+button.addEventListener("click" , function (){
+    const textDue = (numeroKm.value.trim() * 0.21 );
+    generatore.innerText = textDue.toFixed(2) ;
+    generatore.value = "";
+    let scontoTextDue = textDue * 20 / 100;
+    let scontoDefinitivoTextDue = textDue - scontoTextDue;
+    let scontoBigTextDue = textDue * 40 / 100;
+    let scontoBigDefinitivoTextDue = textDue - scontoBigTextDue;
+
+    if (eta.value < max ) {
+    generatore.innerText = scontoDefinitivoTextDue.toFixed(2) ;
+    }
+    else if (eta.value  >= min ) {
+    generatore.innerText = scontoBigDefinitivoTextDue.toFixed(2) ;
+    }
+})
+
+
+
+
 
 
